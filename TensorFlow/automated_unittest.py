e522e9e482d64f07b0483746e3fa0bb45429fdc8
PatrycjaPotepa <ppotepa@student.agh.edu.pl>
2021-05-02 15:46:57 +0200

    Automated Unittest added

---
+++ TensorFlow/automated_unittest.py
@@ -0,0 +1,23 @@
+import main
+import unittest
+import pandas as pd
+
+class Tests(unittest.TestCase):
+    def setUp(self):
+        self.df = pd.read_csv('Archive/MovieGenre.csv', encoding='ISO-8859-1')
+        classes = []
+        for genres in self.df["Genre"].values:
+            try:
+                classes += genres.split('|')
+            except:
+                pass
+        classes = set(classes)
+        self.classes = classes
+
+    def test_genres(self):
+        check = all(i in self.classes for i in main.CLASSES)
+        self.assertTrue(check)
+
+if __name__ == '__main__':
+    unittest.main()
+  
