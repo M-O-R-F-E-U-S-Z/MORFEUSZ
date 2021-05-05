d059f084f1348460409bf83b9ccd9a13466cf7cd
Alicja Szemalikowska <altaira45@gmail.com>
2021-05-05 15:46:37 +0200

    moje

--- morfeusz/morfeusz/settings.py
+++ morfeusz/morfeusz/settings.py
@@ -31,14 +31,15 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'morfeusz_app.apps.MorfeuszAppConfig',
+    'users.apps.UsersConfig',
+    # 'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'morfeusz_app',
-    'users',
 ]
 
 MIDDLEWARE = [
@@ -125,3 +126,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# CRISPY_TEMPLATE_PACK = 'bootstrap4'
