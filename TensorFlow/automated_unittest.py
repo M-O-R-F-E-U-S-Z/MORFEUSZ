c9175e34c4a06b0f06787b83ccfc0dc00e9626a4
PatrycjaPotepa <80523973+PatrycjaPotepa@users.noreply.github.com>
2021-05-07 12:46:52 +0200

    Update automated_unittest.py

--- TensorFlow/automated_unittest.py
+++ TensorFlow/automated_unittest.py
@@ -1,23 +1,9 @@
 import unittest
-import pandas as pd
 import main
 
 class Tests(unittest.TestCase):
-    def setUp(self):
-        self.df = pd.read_csv('Archive/MovieGenre.csv', encoding='ISO-8859-1')
-        classes = []
-        for genres in self.df["Genre"].values:
-            try:
-                classes += genres.split('|')
-            except:
-                pass
-        classes = set(classes)
-        self.classes = classes
-
-    def test_genres(self):
-        check = all(i in self.classes for i in main.CLASSES)
-        self.assertTrue(check)
+    def test_classes(self):
+        self.assertEqual(main.CLASSES, 5)
 
 if __name__ == '__main__':
     unittest.main()
-  
