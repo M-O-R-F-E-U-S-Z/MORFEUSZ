47862a2383571bb7783b36ee9c1fa7f53885a8a0
PatrycjaPotepa <80523973+PatrycjaPotepa@users.noreply.github.com>
2021-05-07 12:49:26 +0200

    Update automated_unittest.py

--- TensorFlow/automated_unittest.py
+++ TensorFlow/automated_unittest.py
@@ -3,7 +3,7 @@ import main
 
 class Tests(unittest.TestCase):
     def test_classes(self):
-        self.assertEqual(main.CLASSES, 5)
+        self.assertEqual(len(main.CLASSES), 5)
 
 if __name__ == '__main__':
     unittest.main()
