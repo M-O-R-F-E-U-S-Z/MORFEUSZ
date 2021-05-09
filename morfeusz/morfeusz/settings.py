cacee75949eb672230edfdf9a35ba44a5fcc1b5d
RafalDamian <rafal.damian.pl@gmail.com>
2021-05-09 19:48:10 +0200

    do heroku i gunicorn

--- morfeusz/morfeusz/settings.py
+++ morfeusz/morfeusz/settings.py
@@ -147,7 +147,7 @@ STATICFILES_DIRS = (
 )
 
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 
