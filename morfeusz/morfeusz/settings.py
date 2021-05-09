996aaec8d9a988a9a287101525b38e25d628505c
RafalDamian <rafal.damian.pl@gmail.com>
2021-05-09 20:16:14 +0200

    do heroku i gunicorn

--- morfeusz/morfeusz/settings.py
+++ morfeusz/morfeusz/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -143,5 +144,4 @@ LOGOUT_REDIRECT_URL = ''
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-
-
+ALLOWED_HOSTS = ['morfeusz.herokuapp.com']
