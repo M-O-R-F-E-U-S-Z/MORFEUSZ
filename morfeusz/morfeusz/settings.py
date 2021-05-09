1088ff8dd198918d56dc9c2ed41defc0a9e0873e
RafalDamian <rafal.damian.pl@gmail.com>
2021-05-09 19:43:44 +0200

    do heroku i gunicorn

--- morfeusz/morfeusz/settings.py
+++ morfeusz/morfeusz/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 
+PROJECT_ROOT   =   os.path.join(os.path.abspath(__file__))
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -148,4 +150,4 @@ STATICFILES_DIRS = (
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 
-PROJECT_ROOT   =   os.path.join(os.path.abspath(_file_))
+
