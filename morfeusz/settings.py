fe1f1cf076d578b1d583b55265f6b6212b71c754
szemala <80889407+szemala@users.noreply.github.com>
2021-05-10 20:54:33 +0200

    Update settings.py

--- morfeusz/settings.py
+++ morfeusz/settings.py
@@ -83,8 +83,11 @@ WSGI_APPLICATION = 'morfeusz.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'd6radno2p6e92q',
+        'USER': 'tsdbbjcaxsgzgn',
+        'PASSWORD': '2b08ccf9330fe60a61859abfeeb075f4020ac83a36d12f37edf56f197973c229',
+        'HOST': 'ec2-54-154-101-45.eu-west-1.compute.amazonaws.com',
     }
 }
 
