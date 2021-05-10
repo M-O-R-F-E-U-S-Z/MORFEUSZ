52a1ffeaba5263a8306629b3703ed1ffe2154947
szemala <80889407+szemala@users.noreply.github.com>
2021-05-10 21:04:23 +0200

    secret key

--- morfeusz/settings.py
+++ morfeusz/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = 'django-insecure-124_9yr!@9b8va32f&qwf)rti^0u2rbp@36)#)*7&zq%e^_j+x'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
