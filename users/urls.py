d36dc8a4c02edf9844a41f81164abbeb02c9d280
Alicja Szemalikowska <altaira45@gmail.com>
2021-05-11 16:41:08 +0200

    more changes to friends

--- users/urls.py
+++ users/urls.py
@@ -10,8 +10,10 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(
         template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(
-        template_name='users/logout.html'), name='logout')
+        template_name='users/logout.html'), name='logout'),
     # path('users/', include('users.urls')),
     # path('', include('morfeusz_app.urls')),
+    #path('views/send_friend_request/<int:user_id/>', views.send_friend_request, name='send_friend_request'),
+    #path('views/accept_friend_request/<int:request_id/>', views.accept_friend_request, name='accept_friend_request'),
 
 ]
