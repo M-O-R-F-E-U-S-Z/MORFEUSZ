9f742ce2fb942a246d748a6628723036b0142532
PatrycjaPotepa <ppotepa@student.agh.edu.pl>
2021-05-18 12:35:54 +0200

    Update main.py

--- morfeusz_app/TensorFlow/main.py
+++ morfeusz_app/TensorFlow/main.py
@@ -3,16 +3,18 @@ import training
 import analysis
 import logging
 
+CLASSES = ['Action', 'Comedy', 'Drama', 'Horror', 'Romance']
+
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
 
-    #data_manager.create_data()
+    data_manager.create_data()
 
     layer_sizes = [128]
     conv_layers = [4]
     dense_layers = [2]
     epochs = 50
-    #training.model(layer_sizes, conv_layers, dense_layers, epochs)
+    training.model(layer_sizes, conv_layers, dense_layers, epochs)
     
     layer_size = layer_sizes[0]
     conv_layer = conv_layers[0]
