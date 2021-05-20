9d2be4d77c8ca9dd32644435660fb4c7862b2fc7
RafalDamian <53561746+RafalDamian@users.noreply.github.com>
2021-05-20 15:29:16 +0200

    dotenv

--- morfeusz/settings.py
+++ morfeusz/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+load_dotenv()
 
 PROJECT_ROOT   =   os.path.join(os.path.abspath(__file__))
 
@@ -23,6 +25,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
 #SECRET_KEY = 'django-insecure-124_9yr!@9b8va32f&qwf)rti^0u2rbp@36)#)*7&zq%e^_j+x'
 
 # SECURITY WARNING: don't run with debug turned on in production!
