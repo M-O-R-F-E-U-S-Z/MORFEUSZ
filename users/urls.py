32e3d24165e48db89559b291a9767cdc369b8d90
KFulat <karolfulat@gmail.com>
2021-05-27 01:23:52 +0200

    Movie qualification

--- users/urls.py
+++ users/urls.py
@@ -11,9 +11,16 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(
         template_name='users/logout.html'), name='logout'),
     path('send_friend_request/', views.send_friend_request, name='send_friend_request'),
-    path('accept_friend_request/<int:request_id>/', views.accept_friend_request, name='accept_friend_request'),
-    path('decline_friend_request/<int:request_id>/', views.decline_friend_request, name='decline_friend_request'),
-    path('cancel_friend_request/<int:request_id>/', views.cancel_friend_request, name='cancel_friend_request'),
+    path('accept_friend_request/<int:request_id>/',
+         views.accept_friend_request, name='accept_friend_request'),
+    path('decline_friend_request/<int:request_id>/',
+         views.decline_friend_request, name='decline_friend_request'),
+    path('cancel_friend_request/<int:request_id>/',
+         views.cancel_friend_request, name='cancel_friend_request'),
     path('unfriend/<int:friend_id>/', views.unfriend, name='unfriend'),
-
+    path('movie_qualification/', views.movie_qualification, name='movie_qualification'),
+    path('movies_dont_like/', views.movies_dont_like, name='movies_dont_like'),
+    path('movies_like_dont_watch/', views.movies_like_dont_watch, name='movies_like_dont_watch'),
+    path('movies_like_watch/', views.movies_like_watch, name='movies_like_watch'),
+    path('movies_watch/', views.movies_watch, name='movies_watch')
 ]
