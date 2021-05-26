622c233007e7a588c3c75834bf891919b970b390
KFulat <karolfulat@gmail.com>
2021-05-27 01:21:43 +0200

    Movie model update

--- morfeusz_app/models.py
+++ morfeusz_app/models.py
@@ -6,26 +6,22 @@ ia = IMDb()
 
 
 class Movie(models.Model):
-    # STATUS_CHOICES = [
-    #     ('N', 'Do not want to watch'),
-    #     ('LN', 'Like and do not want to watch'),
-    #     ('LY', 'Like and want to watch'),
-    #     ('Y', 'Want to watch')
-    # ]
-    # users = models.ManyToManyField(User, related_name='movies')
-    # user = models.ForeignKey(User,
-    # on_delete=models.CASCADE, related_name='movies', null=True)
-    movie_imdb_id = models.CharField(max_length=7, default='0000000')
-    # status = models.CharField(choices=STATUS_CHOICES,
-    #                           default='Y', max_length=255)
+    imdb_id = models.CharField(max_length=7, default='0000000')
     title = models.CharField(default='', max_length=255)
+    genre = models.CharField(default='', max_length=255)
+    cover_url = models.CharField(default='', max_length=255)
 
     class Meta:
-        ordering = ['movie_imdb_id']
+        ordering = ['imdb_id']
 
     def set_title(self):
-        self.title = ia.get_movie(self.movie_imdb_id)['title']
+        self.title = ia.get_movie(self.imdb_id)['title']
+
+    def set_genre(self):
+        self.genre = ia.get_movie(self.imdb_id)['genres'][0]
+
+    def set_cover_url(self):
+        self.cover_url = ia.get_movie(self.imdb_id)['cover url']
 
     def __str__(self):
-        # title = ia.get_movie(self.movie_imdb_id)['title']
         return self.title
