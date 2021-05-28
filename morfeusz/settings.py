3faa0449f8160df1936c934a14b698bf3e67b21a
szemala <80889407+szemala@users.noreply.github.com>
2021-05-28 12:43:31 +0200

    Update settings.py

--- morfeusz/settings.py
+++ morfeusz/settings.py
@@ -81,8 +81,8 @@ TEMPLATES = [
 WSGI_APPLICATION = 'morfeusz.wsgi.application'
 
 
-Database
-https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+# Database
+# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
     'default': {
