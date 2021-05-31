03a2c68bb8ce77c99bc0ea0a70fbe52106ea3e76
szemala <80889407+szemala@users.noreply.github.com>
2021-05-31 21:20:16 +0200

    group system basic functionality is working

--- morfeusz_app/urls.py
+++ morfeusz_app/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='morfeusz_app-home'),
-    path('group/<int:group_id>/', views.manage_group, name='group')
+    path('group/<int:group_id>/', views.manage_group, name='group'),
+    path('delete_from_group/<int:group_id>/<int:member_id>/',
+         views.delete_from_group, name='delete_from_group')
 ]
 
--- morfeusz_app/views.py
+++ morfeusz_app/views.py
@@ -37,30 +37,30 @@ def home(request):
 def manage_group(request, group_id):
     context = {}
     group = Group.objects.get(id=group_id)
-    context['group_name'] = group.name
+    context['group'] = group
     user = request.user
+
+    members = group.members.all()
+    context['members'] = members
+    # print('EEEEEEEEEEEEE MACARENA')
+    # print(request.POST)
     if request.method == "POST":
-        # form = AddGroupMember(request.POST)
-        # print('sadasdasdsadasdasd')
-        # print(request.POST)
-        # print(request.POST['friend'])
         member_to_add = User.objects.get(id=request.POST['friend'])
         group.add_member(member_to_add)
         return redirect('morfeusz_app-home')
-        # print(request)
-        # print(form.cleaned_data.get("friend"))
-        # if form.is_valid():
-        #     # form.save()
-        #     # group.add_member(form.cleaned_data.get("friend"))
-        #     return redirect('morfeusz_app-home')
-        # else:
-        #     return HttpResponse("form is not valid")
     else:
         friends = FriendList.objects.get(user=user)
         friend_list = []
         for index, my_friend in enumerate(friends.friends.all(), start=1):
-            friend_list.append((my_friend, my_friend.username))
+            friend_list.append((my_friend.id, my_friend.username))
         form = AddGroupMember(friend_list)
-
     context['form'] = form
+
     return render(request, 'morfeusz_app/manage_group.html', context)
+
+
+def delete_from_group(request, group_id, member_id):
+    group = Group.objects.get(id=group_id)
+    member = User.objects.get(id=member_id)
+    group.remove_member(member)
+    return redirect('morfeusz_app-home')
