e4b22406fae0a44c3fb592e4d16a076e0efdd167
szemala <altaira45@gmail.com>
2021-06-04 00:01:32 +0200

    pictures working

--- morfeusz/urls.py
+++ morfeusz/urls.py
@@ -25,9 +25,8 @@ urlpatterns = [
     path('users/', include('users.urls')),
     path('', include('morfeusz_app.urls')),
 
-# ] + staticfiles_urlpatterns()
 ]
-# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 else:
