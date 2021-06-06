30e7abb068cbed0afac1db142a9e37f9ce73e32c
kDziadkowiec <41295942+DziaduXD@users.noreply.github.com>
2021-06-07 01:52:57 +0200

    Update forms.py
    added constructor  for userregisterform (to modify field labels)

--- users/forms.py
+++ users/forms.py
@@ -7,6 +7,18 @@ from .models import Profile
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField()
 
+    def __init__(self, *args, **kwargs):
+        super(UserRegisterForm, self).__init__(*args, **kwargs)
+
+        for fieldname in ['username', 'password1', 'password2', 'email']:
+            self.fields[fieldname].help_text = None
+
+        self.fields['username'].label = "Username"
+        self.fields['email'].label = "Email"
+        self.fields['password1'].label = "Password"
+        self.fields['password2'].label = "Confirm Password"
+        self.label_suffix = ""
+    
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
