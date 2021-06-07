6adb0353c2ea7d6492b754573d99c68e9ae47b3a
PatrycjaPotepa <80523973+PatrycjaPotepa@users.noreply.github.com>
2021-06-07 23:55:00 +0200

    Update tests.py

--- morfeusz_app/tests.py
+++ morfeusz_app/tests.py
@@ -1,4 +1,5 @@
 import os
+import sys
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(BASE_DIR)
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'morfeusz.settings')
