b76dadf0efe39f2000038e53db40a30da1ee4e39
PatrycjaPotepa <80523973+PatrycjaPotepa@users.noreply.github.com>
2021-06-08 00:27:45 +0200

    Update tests.py

--- morfeusz_app/tests.py
+++ morfeusz_app/tests.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import django
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(BASE_DIR)
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'morfeusz.settings')
