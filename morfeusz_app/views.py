2d94301881981670991e14456a7f3d3ae802e23d
RafalDamian <53561746+RafalDamian@users.noreply.github.com>
2021-06-10 13:23:54 +0200

    Update views.py

--- morfeusz_app/views.py
+++ morfeusz_app/views.py
@@ -73,9 +73,7 @@ def recomended_movies(request, group_id):
     group = Group.objects.get(id=group_id)
     match = group.movie_matcher()
     max_recomendations = group.MAX_RECOMENDATIONS
-    context = {'results': [], 'titles': [], 'movies': []}
+    context = {'movies': {}}
     for i in range(max_recomendations):
-        context['results'].append(match[i][0])
-        context['titles'].append(match[i][1])
-        context['movies'].append(match[i][2])                                               
+        context['movies'][match[i][0]] = match[i][2]                                    
     return render(request, 'morfeusz_app/recomended_movies.html', context)
