0b198ec7816abf2e4e8470e93fe0da936731fb05
RafalDamian <53561746+RafalDamian@users.noreply.github.com>
2021-06-10 12:12:14 +0200

    Update models.py

--- morfeusz_app/models.py
+++ morfeusz_app/models.py
@@ -59,7 +59,7 @@ def cnn(img):
     #X = np.asarray(bytearray(img.read()), dtype="uint8")
     #X = cv2.imdecode(X, cv2.IMREAD_COLOR)
     #X = cv2.imread(img)
-    req = urllib.urlopen(img)
+    req = urllib.request.urlopen(img)
     arr = np.asarray(bytearray(req.read()), dtype=np.uint8)
     X = cv2.imdecode(arr, -1)
     X = cv2.resize(X, (img_size[0], img_size[1]))
